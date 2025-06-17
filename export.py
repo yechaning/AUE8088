c2e0b7fb23dc623ea01736bda489d429d43b9a40
yechaning <yechani9703@naver.com>
2025-06-17 23:52:40 +0900

    Update export.py

--- export.py
+++ export.py
@@ -2,7 +2,7 @@
 """
 Export a YOLOv5 PyTorch model to other formats. TensorFlow exports authored by https://github.com/zldrobit
 
-Format                      | `export.py --include`         | Model
+Format                      | `export.py --include`         | Model.
 ---                         | ---                           | ---
 PyTorch                     | -                             | yolov5s.pt
 TorchScript                 | `torchscript`                 | yolov5s.torchscript
